ae60bdf935ac1e13425c44e6a2b583baee18090a
hydrz <n.haoyuan@gmail.com>
2023-02-25 14:41:54 +0800

    'w'

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--listen --enable-insecure-extension-access --medvram --opt-sub-quad-attention --disable-nan-check --no-half --theme dark --api --cors-allow-origins http://localhost:5173
+set COMMANDLINE_ARGS=--opt-sub-quad-attention --disable-nan-check --no-half --theme dark --medvram --listen --enable-insecure-extension-access
 
 call webui.bat
